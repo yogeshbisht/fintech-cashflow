995088e03859d9e69575f98f6a0def0507c15f84
Yogesh Bisht <reinzire@gmail.com>
2023-09-28 12:03:38 +0530

    register page and form component

--- app/(auth)/(routes)/register/components/register-form.tsx
+++ app/(auth)/(routes)/register/components/register-form.tsx
@@ -17,30 +17,33 @@ import { Input } from "@/components/ui/input";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 const formSchema = z.object({
   email: z.string().email(),
   password: z.string().min(8),
-  rememberMe: z.boolean().optional(),
+  confirmPassword: z.string().min(8),
+  agreeTerms: z.boolean().optional(),
 });
 
-type SignInFormValues = z.infer<typeof formSchema>;
+type RegisterFormValues = z.infer<typeof formSchema>;
 
-const SignInForm = () => {
+const RegisterForm = () => {
   const router = useRouter();
 
   const [loading, setLoading] = useState(false);
 
-  const form = useForm<SignInFormValues>({
+  const form = useForm<RegisterFormValues>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       email: "",
       password: "",
-      rememberMe: false,
+      confirmPassword: "",
+      agreeTerms: false,
     },
   });
 
-  const onSubmit = async (values: SignInFormValues) => {
+  const onSubmit = async (values: RegisterFormValues) => {
     setLoading(true);
     console.log(values);
     setLoading(false);
@@ -71,7 +74,7 @@ const SignInForm = () => {
             control={form.control}
             name="password"
             render={({ field }) => (
-              <FormItem className="mb-6">
+              <FormItem className="mb-4">
                 <FormControl>
                   <Input
                     className="dark:bg-gray-950 dark:placeholder:text-gray-700 dark:text-white/80 ease-soft bg-white text-gray-700 transition-all focus:border-gray-300"
@@ -86,14 +89,37 @@ const SignInForm = () => {
           />
           <FormField
             control={form.control}
-            name="rememberMe"
+            name="confirmPassword"
+            render={({ field }) => (
+              <FormItem className="mb-6">
+                <FormControl>
+                  <Input
+                    className="dark:bg-gray-950 dark:placeholder:text-gray-700 dark:text-white/80 ease-soft bg-white text-gray-700 transition-all focus:border-gray-300"
+                    disabled={loading}
+                    placeholder="Confirm Password"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="agreeTerms"
             render={({ field }) => (
               <FormItem className="flex justify-start items-center">
                 <FormControl>
                   <Checkbox checked={field.value} disabled={loading} />
                 </FormControl>
                 <FormLabel className="text-xs text-slate-400 pl-2 pb-2">
-                  Remember Me
+                  I agree the{" "}
+                  <Link
+                    href="/terms-and-conditions"
+                    className="font-bold text-slate-500"
+                  >
+                    Terms and Conditions
+                  </Link>
                 </FormLabel>
               </FormItem>
             )}
@@ -102,28 +128,20 @@ const SignInForm = () => {
             disabled={loading}
             className="w-full mt-8 font-semibold text-white uppercase transition-all bg-transparent active:opacity-85 hover:scale-102 hover:shadow-soft-xs leading-pro text-xs bg-gradient-to-tl from-blue-600 to-cyan-400 hover:border-slate-700 hover:bg-slate-700 hover:text-white"
           >
-            Login
+            Register
           </Button>
         </form>
       </Form>
-      <div className="relative w-full max-w-full p-6 my-2 text-center shrink-0">
-        <p className="text-slate-400 text-sm mb-0 font-semibold before:bg-gradient-to-r before:from-transparent before:via-neutral-500/40 before:to-neutral-500/40 before:right-2 before:-ml-1/2 before:content-[''] before:inline-block before:w-3/10 before:h-px before:relative before:align-middle after:left-2 after:-mr-1/2 after:bg-gradient-to-r after:from-neutral-500/40 after:via-neutral-500/40 after:to-transparent after:content-[''] after:inline-block after:w-3/10 after:h-px after:relative after:align-middle">
-          Don&apos;t have an account?
-        </p>
-      </div>
       <div className="text-center">
-        <Button
-          disabled={loading}
-          className="w-full mb-4 font-semibold text-white uppercase transition-all bg-transparent active:opacity-85 hover:scale-102 hover:shadow-soft-xs leading-pro text-xs bg-gradient-to-tl from-gray-900 to-slate-800 hover:border-slate-700 hover:bg-slate-700 hover:text-white"
-          onClick={() => {
-            router.push("/register");
-          }}
-        >
-          Register
-        </Button>
+        <p className="py-8 mb-0 leading-normal text-sm">
+          Already have an account?{" "}
+          <Link href="/login" className="font-bold text-slate-400">
+            Sign in
+          </Link>
+        </p>
       </div>
     </>
   );
 };
 
-export default SignInForm;
+export default RegisterForm;
